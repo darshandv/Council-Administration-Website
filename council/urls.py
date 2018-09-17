2d12465f8ee86b51cae860bdd0e60084a7a66681
Darshan D V <darshandv10@gmail.com>
2018-09-17 16:49:25 +0530

    User attributes added

--- council/urls.py
+++ council/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('council.urls')),
+    path('',views.index,name='index'),
 ]
