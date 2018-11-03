ba9c8e94cca547cc776114afc9300a15d75123a0
Darshan D V <darshandv10@gmail.com>
2018-11-03 11:43:56 +0530

    Added index page

--- council/views.py
+++ council/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate,login
 
 # Create your views here.
 def index(request):
-    return render(request,'council/admin.html')
+    return render(request,'council/index.html')
 
 def signup(request):
     if request.method == 'POST':
--- council_website/settings.py
+++ council_website/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
